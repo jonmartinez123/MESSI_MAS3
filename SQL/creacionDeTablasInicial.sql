10a0fd4e4d873d696bb27713d2003ef7ef799388
jonmartinez123 <jongmartinez95@gmail.com>
2016-05-16 23:06:01 -0300

    Agregado migracion de calificaciones

--- SQL/creacionDeTablasInicial.sql
+++ SQL/creacionDeTablasInicial.sql
@@ -1,7 +1,7 @@
 USE[GD1C2016]
 GO
 
-CREATE SCHEMA [MESSI_MAS3] AUTHORIZATION [gd]
+--CREATE SCHEMA [MESSI_MAS3] AUTHORIZATION [gd]
 
 -- -----------------------------------------------------
 -- Table MESSI_MAS3.Funcionalidad
@@ -18,9 +18,9 @@ CREATE TABLE MESSI_MAS3.Funcionalidad (
 -- -----------------------------------------------------
 CREATE TABLE MESSI_MAS3.Usuario (
   usuario_id INT PRIMARY KEY NOT NULL IDENTITY,
-  usuario_nombreUsuario NVARCHAR(255) NOT NULL UNIQUE,
+  usuario_nombreUsuario NVARCHAR(255) NOT NULL,  --AGREGAR UNIQUE!!!
   usuario_contrasenia NVARCHAR(30) NULL,				--Cambiado de NOT NULL a NULL y el nombre a 'contrasenia'
-  usuario_mail NVARCHAR(50) NOT NULL UNIQUE,
+  usuario_mail NVARCHAR(50) NOT NULL UNIQUE,	--LINEA PELIGROSA VIGILAR!!
   usuario_deleted INT DEFAULT 0,
   usuario_intentos INT DEFAULT 0, )
  
@@ -53,7 +53,7 @@ CREATE TABLE MESSI_MAS3.Localidad (
 -- -----------------------------------------------------
 CREATE TABLE MESSI_MAS3.Domicilio (
   domicilio_idDomicilio INT PRIMARY KEY NOT NULL IDENTITY,
-  domicilio_localidad_id INT REFERENCES Localidad(Localidad_id),
+  domicilio_localidad_id INT REFERENCES MESSI_MAS3.Localidad(Localidad_id),
   domicilio_calle NVARCHAR(100) NOT NULL,
   domicilio_altura NUMERIC(18,0) NOT NULL,
   domicilio_piso NUMERIC(18,0) NOT NULL,
@@ -73,8 +73,8 @@ CREATE TABLE MESSI_MAS3.Persona (
   persona_DNI NUMERIC(18,0) NOT NULL UNIQUE,
   persona_fechaNacimiento DATETIME NOT NULL,
   persona_fechaCreacion DATETIME NOT NULL,
-  persona_idDomicilio INT REFERENCES Domicilio(domicilio_idDomicilio),
-  persona_id INT PRIMARY KEY REFERENCES Usuario(usuario_id),
+  persona_idDomicilio INT REFERENCES MESSI_MAS3.Domicilio(domicilio_idDomicilio),
+  persona_id INT PRIMARY KEY REFERENCES MESSI_MAS3.Usuario(usuario_id),
   persona_tel NUMERIC(15,0) DEFAULT 0,
   persona_mail NVARCHAR(255) NOT NULL,
   )
@@ -99,12 +99,12 @@ CREATE TABLE MESSI_MAS3.Rubro (
 CREATE TABLE MESSI_MAS3.Empresa (
   empresa_razonSocial NVARCHAR(255) NOT NULL,
   empresa_cuit NVARCHAR(50) NOT NULL,
-  empresa_id INT PRIMARY KEY REFERENCES Usuario (usuario_id),
+  empresa_id INT PRIMARY KEY REFERENCES MESSI_MAS3.Usuario (usuario_id),
   empresa_calificacionPromedio INT NULL,						--Cambio de Not null a NULL
   empresa_fechaCreacion DATETIME NOT NULL,
-  empresa_idDomicilio INT REFERENCES Domicilio (domicilio_idDomicilio),
+  empresa_idDomicilio INT REFERENCES MESSI_MAS3.Domicilio (domicilio_idDomicilio),
   empresa_telefono VARCHAR(10) NOT NULL,
-  empresa_rubro INT REFERENCES Rubro (rubro_id),)
+  empresa_rubro INT REFERENCES MESSI_MAS3.Rubro (rubro_id),)
  
 -- -----------------------------------------------------
 -- Table MESSI_MAS3.Estado
@@ -134,9 +134,9 @@ CREATE TABLE MESSI_MAS3.Visibilidad (
 -- -----------------------------------------------------
 CREATE TABLE MESSI_MAS3.Publicacion (
   publicacion_id INT PRIMARY KEY NOT NULL IDENTITY,
-  publicacion_idEstado INT REFERENCES Estado (estado_id),
-  publicacion_idVisibilidad INT REFERENCES Visibilidad (visibilidad_id),
-  publicacion_idUsuario INT REFERENCES Usuario (usuario_id),
+  publicacion_idEstado INT REFERENCES MESSI_MAS3.Estado (estado_id),
+  publicacion_idVisibilidad INT REFERENCES MESSI_MAS3.Visibilidad (visibilidad_id),
+  publicacion_idUsuario INT REFERENCES MESSI_MAS3.Usuario (usuario_id),
   publicacion_fechaInicio DATETIME NOT NULL,
   publicacion_fechaFin DATETIME NOT NULL,
   publicacion_descripcion VARCHAR(255) NULL,
@@ -155,23 +155,23 @@ CREATE TABLE MESSI_MAS3.Publicacion (
 -- Table MESSI_MAS3.Rubro_x_Publicacion
 -- -----------------------------------------------------
 CREATE TABLE MESSI_MAS3.Rubro_x_Publicacion (
-  idPublicacion INT REFERENCES Publicacion (publicacion_id),
-  idRubro INT REFERENCES Rubro (rubro_id) ,)
+  idPublicacion INT REFERENCES MESSI_MAS3.Publicacion (publicacion_id),
+  idRubro INT REFERENCES MESSI_MAS3.Rubro (rubro_id) ,)
   
 -- -----------------------------------------------------
 -- Table MESSI_MAS3.RolUsuario
 -- -----------------------------------------------------
 CREATE TABLE MESSI_MAS3.Rol_Usuario (
-  Usuario_id INT REFERENCES Usuario (usuario_id),
-  Rol_id INT REFERENCES Rol (rol_id),
+  Usuario_id INT REFERENCES MESSI_MAS3.Usuario (usuario_id),
+  Rol_id INT REFERENCES MESSI_MAS3.Rol (rol_id),
 )
 
 -- -----------------------------------------------------
 -- Table MESSI_MAS3.FuncionalidadRol
 -- -----------------------------------------------------
 CREATE TABLE MESSI_MAS3.Funcionalidad_Rol (
-  Rol_id INT REFERENCES Rol(rol_id),
-  Funcionalidad_id INT REFERENCES Funcionalidad (funcionalidad_id),
+  Rol_id INT REFERENCES MESSI_MAS3.Rol(rol_id),
+  Funcionalidad_id INT REFERENCES MESSI_MAS3.Funcionalidad (funcionalidad_id),
   )
 
 -- -----------------------------------------------------
@@ -179,9 +179,9 @@ CREATE TABLE MESSI_MAS3.Funcionalidad_Rol (
 -- -----------------------------------------------------
 CREATE TABLE MESSI_MAS3.Compra (
   compra_id INT PRIMARY KEY NOT NULL IDENTITY,
-  compras_publicacion_id INT REFERENCES Publicacion (publicacion_id),
+  compras_publicacion_id INT REFERENCES MESSI_MAS3.Publicacion (publicacion_id),
   compras_fecha DATETIME NOT NULL,
-  compras_personaComprador_id INT REFERENCES Persona(persona_id),
+  compras_personaComprador_id INT REFERENCES MESSI_MAS3.Persona(persona_id),
   compras_cantidad NUMERIC(18,0) NOT NULL, )
  
 -- -----------------------------------------------------
@@ -190,8 +190,8 @@ CREATE TABLE MESSI_MAS3.Compra (
 CREATE TABLE MESSI_MAS3.Oferta (
   oferta_id INT PRIMARY KEY NOT NULL IDENTITY,
   oferta_valor NUMERIC(18,2) NOT NULL,
-  oferta_persona_id INT REFERENCES Persona(persona_id),						
-  oferta_idPublicacion INT REFERENCES Publicacion (publicacion_id),
+  oferta_persona_id INT REFERENCES MESSI_MAS3.Persona(persona_id),						
+  oferta_idPublicacion INT REFERENCES MESSI_MAS3.Publicacion (publicacion_id),
   oferta_fecha DATETIME NOT NULL,
   oferta_ganador INT DEFAULT 0,
   )
@@ -202,13 +202,13 @@ CREATE TABLE MESSI_MAS3.Oferta (
 -- -----------------------------------------------------
 CREATE TABLE MESSI_MAS3.Calificacion (
   calificacion_id INT PRIMARY KEY NOT NULL IDENTITY,
-  calificacion_idPersonaCalificador INT REFERENCES Persona(persona_id), 
-  calificacion_compraId INT REFERENCES Compra(compra_id),
+  calificacion_idPersonaCalificador INT REFERENCES MESSI_MAS3.Persona(persona_id), 
+  calificacion_compraId INT REFERENCES MESSI_MAS3.Compra(compra_id),
   calificacion_fecha DATETIME NULL,
   calificacion_cantidadEstrellas NUMERIC(18,0) NULL,
   calificacion_detalle NVARCHAR(255) NULL,
   calificacion_pendiente INT DEFAULT 0 NOT NULL,					--ACA!!!
-  calificacion_idusuarioCalificado INT REFERENCES Usuario(usuario_id),
+  calificacion_idusuarioCalificado INT REFERENCES MESSI_MAS3.Usuario(usuario_id),
 )
  
 
@@ -229,9 +229,9 @@ CREATE TABLE MESSI_MAS3.Factura (
   factura_id INT PRIMARY KEY NOT NULL IDENTITY,
   factura_fecha DATETIME NOT NULL,
   factura_importeTotal NUMERIC(18,2) NOT NULL,
-  factura_idVendedor INT REFERENCES Usuario (usuario_id),
+  factura_idVendedor INT REFERENCES MESSI_MAS3.Usuario (usuario_id),
   factura_numero NUMERIC(18,0) NOT NULL,
-  factura_formaDePago INT REFERENCES FormaDePago (formaDePago_id),
+  factura_formaDePago INT REFERENCES MESSI_MAS3.FormaDePago (formaDePago_id),
   
 )
 
@@ -241,8 +241,8 @@ CREATE TABLE MESSI_MAS3.Factura (
 CREATE TABLE MESSI_MAS3.Pregunta (
   pregunta_id INT PRIMARY KEY NOT NULL IDENTITY,
   pregunta NVARCHAR(255) NOT NULL,
-  pregunta_idPublicacion INT REFERENCES Publicacion (publicacion_id),
-  pregunta_idPersonaPreguntador INT REFERENCES Persona (persona_id), 
+  pregunta_idPublicacion INT REFERENCES MESSI_MAS3.Publicacion (publicacion_id),
+  pregunta_idPersonaPreguntador INT REFERENCES MESSI_MAS3.Persona (persona_id), 
   pregunta_fecha DATETIME NOT NULL, )
  
 
@@ -253,10 +253,10 @@ CREATE TABLE MESSI_MAS3.Pregunta (
 -- -----------------------------------------------------
 CREATE TABLE MESSI_MAS3.Respuesta (
   respuesta_id INT PRIMARY KEY NOT NULL IDENTITY,
-  respuesta_idPregunta INT REFERENCES Pregunta (pregunta_id),
+  respuesta_idPregunta INT REFERENCES MESSI_MAS3.Pregunta (pregunta_id),
   respuesta_fecha DATETIME NULL,
   respuesta_textorespuesta NVARCHAR(255) NULL,
-  respuesta_idUsQueResponde INT REFERENCES Usuario (usuario_id),
+  respuesta_idUsQueResponde INT REFERENCES MESSI_MAS3.Usuario (usuario_id),
   
   )
 
@@ -269,13 +269,13 @@ CREATE TABLE MESSI_MAS3.Factura_detalle (
   FacturaDetalle_valorItem NUMERIC(18,2) NOT NULL,
   facturaDetalle_numero NUMERIC(18,0) NOT NULL,
   facturaDetalle_item NVARCHAR(255) NOT NULL,
-  facturaDetalle_id INT PRIMARY KEY REFERENCES Factura (factura_id),
+  facturaDetalle_id INT PRIMARY KEY REFERENCES MESSI_MAS3.Factura (factura_id),
   facturaDetall_cantidadItems NUMERIC(18,0) NOT NULL,
   
  )
 
  GO
-
+ 
  -- -----------------------------------------------------
 -- PROCEDURES
 -- -----------------------------------------------------
@@ -405,7 +405,7 @@ AS BEGIN
 
 	WHILE(@@FETCH_STATUS = 0)
 		BEGIN
-			EXECUTE MESSI_MAS3.generarUsuario @documento,NULL,@mail,@ultimoIdInsertado = @idUsuario OUTPUT; --esta bien esta linea?? por el documento pasado como param
+			EXECUTE MESSI_MAS3.generarUsuario @documento,'123',@mail,@ultimoIdInsertado = @idUsuario OUTPUT; --esta bien esta linea?? por el documento pasado como param
 			DECLARE @idRol int;
 			SET @idRol = (select rol_id from MESSI_MAS3.Rol where rol_nombre = 'Cliente')
 			INSERT INTO MESSI_MAS3.Rol_Usuario(Rol_id,Usuario_id)
@@ -613,4 +613,59 @@ AS BEGIN
 END
 GO
 
+--CONTINUAR CON ESTO
+CREATE PROCEDURE [MESSI_MAS3].[migrarCalificaciones]
+AS BEGIN
+	set nocount on;
+	set xact_abort on;
+	DECLARE 
+		@id	INT,
+		@idUsuarioCalificador INT,
+		@idPublicacion INT,
+		@fecha DATETIME,
+		@valor INT,
+		@detalle NVARCHAR(45),
+		@pendiente INT
+	DECLARE cur CURSOR FOR
+	
+	SELECT DISTINCT
+		Calificacion_Codigo,	
+		Publicacion_Fecha_Venc,
+		Calificacion_Cant_Estrellas,
+		Calificacion_Descripcion
+	FROM gd_esquema.Maestra	
+	
+	OPEN cur
+	FETCH NEXT FROM cur
+	INTO 
+			@id,
+			@fecha,
+			@valor,
+			@detalle
+	WHILE(@@FETCH_STATUS = 0)
+	BEGIN
+		-- SET @id 
+		SET @idUsuarioCalificador = (SELECT id FROM MESSI_MAS3.Usuario WHERE (SELECT Publ_Cli_Dni from gd_esquema.Maestra) = usuario or (SELECT Publ_Empresa_Cuit from gd_esquema.Maestra) = usuario )
+		SET @pendiente = 1
+		
+		INSERT INTO 
+		MESSI_MAS3.Calificacion(idPublicacion,	
+		fecha,
+		valor,
+		detalle)
+		VALUES (@idPublicacion,
+		 @fecha, 
+		 @valor,
+		 @detalle)
+
+		FETCH NEXT FROM cur
+		INTO @idPublicacion,
+		 @fecha, 
+		 @valor,
+		 @detalle
+	END
+	CLOSE cur 
+	DEALLOCATE cur
+END
+GO
 
