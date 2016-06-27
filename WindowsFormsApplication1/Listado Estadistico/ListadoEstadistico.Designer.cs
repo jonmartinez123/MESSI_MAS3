378d451c12d484ed3c766ca1feaa1dc2a68c3f73
jonmartinez123 <jongmartinez95@gmail.com>
2016-06-27 16:11:10 -0300

    Listado terminado, falta testear

--- WindowsFormsApplication1/Listado Estadistico/ListadoEstadistico.Designer.cs
+++ WindowsFormsApplication1/Listado Estadistico/ListadoEstadistico.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
@@ -37,10 +39,18 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.btnVolver = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rubros = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.visibilidades = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -56,6 +66,23 @@
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Fecha a consultar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Trimestre";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(60, 46);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(150, 21);
+            this.comboBox2.TabIndex = 3;
             // 
             // label1
             // 
@@ -86,41 +113,41 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(576, 226);
+            this.button1.Location = new System.Drawing.Point(579, 264);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(144, 54);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Vendedores con mayor cantidad de productos no vendidos";
+            this.button1.Text = "1-Vendedores con mayor cantidad de productos no vendidos";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(576, 286);
+            this.button2.Location = new System.Drawing.Point(579, 324);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(144, 54);
             this.button2.TabIndex = 2;
-            this.button2.Text = "Clientes con mayor cantidad de productos comprados";
+            this.button2.Text = "2-Clientes con mayor cantidad de productos comprados";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(576, 346);
+            this.button3.Location = new System.Drawing.Point(579, 384);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(144, 54);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Vendedores con mayor cantidad de facturas ";
+            this.button3.Text = "3-Vendedores con mayor cantidad de facturas ";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(576, 406);
+            this.button4.Location = new System.Drawing.Point(579, 444);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(144, 54);
             this.button4.TabIndex = 2;
-            this.button4.Text = "Vendedores con mayor monto facturado";
+            this.button4.Text = "4-Vendedores con mayor monto facturado";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
@@ -134,28 +161,106 @@
             this.btnVolver.UseVisualStyleBackColor = true;
             this.btnVolver.Click += new System.EventHandler(this.btnVolver_Click);
             // 
-            // label2
+            // groupBox2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 49);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(50, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Trimestre";
+            this.groupBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.groupBox2.Controls.Add(this.visibilidades);
+            this.groupBox2.Controls.Add(this.rubros);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(313, 77);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(308, 87);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Filtros posibles";
             // 
-            // comboBox2
+            // label3
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(60, 46);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(150, 21);
-            this.comboBox2.TabIndex = 3;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Rubro:";
+            // 
+            // rubros
+            // 
+            this.rubros.FormattingEnabled = true;
+            this.rubros.Location = new System.Drawing.Point(66, 19);
+            this.rubros.Name = "rubros";
+            this.rubros.Size = new System.Drawing.Size(204, 21);
+            this.rubros.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Visibilidad";
+            // 
+            // visibilidades
+            // 
+            this.visibilidades.FormattingEnabled = true;
+            this.visibilidades.Location = new System.Drawing.Point(66, 51);
+            this.visibilidades.Name = "visibilidades";
+            this.visibilidades.Size = new System.Drawing.Size(204, 21);
+            this.visibilidades.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.Window;
+            this.label5.Location = new System.Drawing.Point(576, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(172, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Atencion: El filtro de Visibilidad solo";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Window;
+            this.label6.Location = new System.Drawing.Point(627, 235);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(120, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "aplica para el Listado 2°";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.Window;
+            this.label7.Location = new System.Drawing.Point(576, 222);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(158, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Atencion: El filtro de Rubro solo ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.Window;
+            this.label8.Location = new System.Drawing.Point(627, 183);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(120, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "aplica para el Listado 1°";
+            this.label8.Click += new System.EventHandler(this.label5_Click);
             // 
             // ListadoEstadistico
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(777, 607);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.btnVolver);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -171,7 +276,10 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -188,5 +296,14 @@
         private System.Windows.Forms.Button btnVolver;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox visibilidades;
+        private System.Windows.Forms.ComboBox rubros;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
--- WindowsFormsApplication1/Listado Estadistico/ListadoEstadistico.cs
+++ WindowsFormsApplication1/Listado Estadistico/ListadoEstadistico.cs
@@ -33,7 +33,14 @@ namespace MercadoEnvio.Listado_Estadistico
             comboBox2.Items.Add("Octubre - Diciembre");
             comboBox1.SelectedIndex = 1;
             comboBox2.SelectedIndex = 1;
+
+            this.agregarVisibilidades(DAO.ListadoSQL.getVisibilidades());
+            this.agregarRubros(DAO.RubroSQL.getRubros());
+            visibilidades.SelectedIndex = 1;
+            rubros.SelectedIndex = 1;
         }
+
+
 
         private void btnVolver_Click(object sender, EventArgs e)
         {
@@ -42,23 +49,44 @@ namespace MercadoEnvio.Listado_Estadistico
             this.Close();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void agregarRubros(List<Modelo.Rubro> list)
+        {
+            foreach (Modelo.Rubro elem in list)
+            {
+                this.rubros.Items.Add(elem.DescripcionCorta);
+            }
+        }
 
+        private void agregarVisibilidades(List<Modelo.Visibilidad> list)
+        {
+            foreach (Modelo.Visibilidad elem in list)
+            {
+                this.visibilidades.Items.Add(elem.Descripcion);
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
         {
             dataGridView1.DataSource = null;
             dataGridView1.Refresh();
             //listado vendedores con mayor cantidad de productos no vendidos
             if (dataGridView1.Columns.Count > 0)
-            { 
+            {
                 int cantidadColumnas = dataGridView1.Columns.Count;
-               for (int i = 1; i <= cantidadColumnas; i++)
-                     {
-                         dataGridView1.Columns.RemoveAt(i);
-                        }
-                
+                for (int i = 1; i <= cantidadColumnas; i++)
+                {
+                    dataGridView1.Columns.RemoveAt(i);
                 }
-            dataGridView1.Columns.Add("column1","Nombre usuario");
-            dataGridView1.Columns.Add("column2","Cantidad productos no vendidos");
+
+            }
+            dataGridView1.Columns.Add("column1", "Usuario ID");
+            dataGridView1.Columns.Add("column2", "Cantidad productos no vendidos");
+            dataGridView1.Columns.Add("column3", "Nombre/Razon social");
+            string anio = comboBox1.SelectedItem.ToString();
+            string visibilidad = visibilidades.SelectedItem.ToString();
+            DAO.ListadoSQL.get_top5vendedoresConMayorCantidadDeProductosNoVendidos(dataGridView1, anio, comboBox2.SelectedIndex, visibilidad);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -74,7 +102,14 @@ namespace MercadoEnvio.Listado_Estadistico
                 }
 
             }
-
+            dataGridView1.Columns.Add("column1", "Cliente ID");
+            dataGridView1.Columns.Add("column2", "DNI");
+            dataGridView1.Columns.Add("column3", "Nombre");
+            dataGridView1.Columns.Add("column4", "Apellido");
+            dataGridView1.Columns.Add("column5", "Cantidad Compras");
+            string anio = comboBox1.SelectedItem.ToString();
+            string rubro = rubros.SelectedItem.ToString();
+            DAO.ListadoSQL.get_top5clientesConMasCompraSegunRubro(dataGridView1, anio, comboBox2.SelectedIndex, rubro);
 
         }
 
@@ -92,8 +127,9 @@ namespace MercadoEnvio.Listado_Estadistico
                 }
 
             }
-            dataGridView1.Columns.Add("column1", "Nombre usuario");
-            dataGridView1.Columns.Add("column2", "Cantidad facturas");
+            dataGridView1.Columns.Add("column1", "Usuario ID");
+            dataGridView1.Columns.Add("column2", "Nombre/Razon social");
+            dataGridView1.Columns.Add("column3", "Cantidad facturas");
             string anio = comboBox1.SelectedItem.ToString();
             DAO.ListadoSQL.getTop5VendedoresConMasFacturas(dataGridView1, anio, comboBox2.SelectedIndex);
 
@@ -114,9 +150,18 @@ namespace MercadoEnvio.Listado_Estadistico
                 }
 
             }
+
+            dataGridView1.Columns.Add("column1", "Usuario ID");
+            dataGridView1.Columns.Add("column2", "Nombre/Razon social");
+            dataGridView1.Columns.Add("column3", "Monto facturado");
             string anio = comboBox1.SelectedItem.ToString();
             DAO.ListadoSQL.getTop5ConMontoMasFacturado(dataGridView1, anio, comboBox2.SelectedIndex);
 
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
 
         }
     }
