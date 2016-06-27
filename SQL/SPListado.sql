548deb7ef374c6c8528e98029d444b1a455052d2
jonmartinez123 <jongmartinez95@gmail.com>
2016-06-27 00:39:04 -0300

    Falta 1 solo SP para el listado, casi terminado. Pensar logica de como elige el rubro

--- SQL/SPListado.sql
+++ SQL/SPListado.sql
@@ -1,42 +1,167 @@
 CREATE PROCEDURE [MESSI_MAS3].get_top5ConMasFacturas(@anio NVARCHAR(255), @nroTrimestre INT)
 AS BEGIN
-DECLARE @fechaInicio DATETIME, @fechaFin DATETIME
+DECLARE @mes1 INT
+DECLARE @mes2 INT
+DECLARE @mes3 INT
+DECLARE @anioCasteado int
+SET @anioCasteado = CONVERT(INT, @anio)
 
-if (@nroTrimestre = 1)
-BEGIN 
-		SET @fechaInicio = CONCAT('01/01/', @anio)
-		SET @fechaFin = CONCAT('31/03/', @anio)
+IF @nroTrimestre = 1 
+BEGIN
+	SET @mes1 = 1
+	SET @mes2 = 2 
+	SET @mes3 = 3
 END
-if (@nroTrimestre = 2)
-BEGIN 
-		SET @fechaInicio = CONCAT('01/04/', @anio)
-		SET @fechaFin = CONCAT('30/06/', @anio)
+IF @nroTrimestre = 2
+BEGIN
+	SET @mes1 = 4
+	SET @mes2 = 5 
+	SET @mes3 = 6
 END
-if (@nroTrimestre = 3)
-BEGIN 
-		SET @fechaInicio = CONCAT('01/07/', @anio)
-		SET @fechaFin = CONCAT('30/09/', @anio)
+IF @nroTrimestre = 3
+BEGIN
+	SET @mes1 = 7
+	SET @mes2 = 8 
+	SET @mes3 = 9
 END
-if (@nroTrimestre = 4)
-BEGIN 
-		SET @fechaInicio = CONCAT('01/10/', @anio)
-		SET @fechaFin = CONCAT('31/12/', @anio)
+IF @nroTrimestre = 4
+BEGIN
+	SET @mes1 = 10
+	SET @mes2 = 11
+	SET @mes3 = 12
 END
 
+SELECT TOP 5	cliempresa.cliente_id	AS idUsuario,
+				usu.usuario_nombreUsuario		AS usuario,
+				cliempresa.cliente_apellido	AS nombre,
+				count (*)		AS cantidadDeFacturas
+FROM				MESSI_MAS3.Factura fac
+		LEFT JOIN	MESSI_MAS3.Publicacion pub ON fac.factura_publicacionId = pub.publicacion_id
+		LEFT JOIN  (SELECT cliente_id, cliente_apellido FROM MESSI_MAS3.Cliente
+					UNION
+					SELECT empresa_id, empresa_razonSocial FROM MESSI_MAS3.Empresa) AS cliempresa ON cliempresa.cliente_id = pub.publicacion_idUsuario
+		LEFT JOIN	MESSI_MAS3.Usuario usu ON pub.publicacion_idUsuario = usu.usuario_id
+WHERE	(MONTH(fac.factura_fecha) = @mes1 OR MONTH(fac.factura_fecha) = @mes2 OR MONTH(fac.factura_fecha) = @mes3)
+		AND
+		YEAR(fac.factura_fecha) = @anio
+GROUP BY cliempresa.cliente_id, cliempresa.cliente_apellido, usu.usuario_nombreUsuario
+ORDER BY cantidadDeFacturas DESC
 
-(SELECT TOP 5  SUM(FacturasEnTrimestre.factura_importeTotal) Monto  
-	FROM (SELECT * FROM MESSI_MAS3.Factura WHERE CONVERT(DATE, factura_fecha, 5) BETWEEN CONVERT(DATE,'2015/01/01') AND CONVERT(DATE,'2015/03/31')) AS FacturasEnTrimestre, MESSI_MAS3.Usuario 
-		WHERE (FacturasEnTrimestre.factura_idVendedor = usuario_id AND EXISTS (SELECT * FROM (SELECT * FROM MESSI_MAS3.Factura WHERE CONVERT(DATE,factura_fecha) BETWEEN CONVERT(DATE,'2015/01/01') AND CONVERT(DATE,'2015/03/31')) AS Factus WHERE factura_idVendedor = usuario_id) )
 
-		) ORDER BY Monto
-
-		/*
-		(SELECT TOP 5  SUM(FacturasEnTrimestre.factura_importeTotal) Monto  
-	FROM (SELECT * FROM MESSI_MAS3.Factura WHERE CONVERT(DATE, factura_fecha, 5) BETWEEN CONVERT(DATE,'2015/01/01') AND CONVERT(DATE,'2015/03/31')) AS FacturasEnTrimestre, MESSI_MAS3.Usuario
-		WHERE ( factura_idVendedor= usuario_id AND  EXISTS (SELECT * FROM (SELECT * FROM MESSI_MAS3.Factura WHERE CONVERT(DATE,factura_fecha) BETWEEN CONVERT(DATE,'2015/01/01') AND CONVERT(DATE,'2015/03/31')) AS Factus WHERE factura_idVendedor = usuario_id) )
-
-		) 
-		
-		*/
 END
 GO  
+
+
+
+CREATE PROCEDURE get_top5ConMayorMontoFacturado(@anio NVARCHAR(255), @nroTrimestre INT)
+AS BEGIN
+
+
+DECLARE @mes1 INT
+DECLARE @mes2 INT
+DECLARE @mes3 INT
+DECLARE @anioCasteado int
+SET @anioCasteado = CONVERT(INT,@anio)
+
+IF @nroTrimestre = 1 
+BEGIN
+	SET @mes1 = 1
+	SET @mes2 = 2 
+	SET @mes3 = 3
+END
+IF @nroTrimestre = 2
+BEGIN
+	SET @mes1 = 4
+	SET @mes2 = 5 
+	SET @mes3 = 6
+END
+IF @nroTrimestre = 3
+BEGIN
+	SET @mes1 = 7
+	SET @mes2 = 8 
+	SET @mes3 = 9
+END
+IF @nroTrimestre = 4
+BEGIN
+	SET @mes1 = 10
+	SET @mes2 = 11
+	SET @mes3 = 12
+END
+
+SELECT TOP 5	cliempresa.cliente_id,			
+				users.usuario_nombreUsuario,				
+				cliempresa.cliente_apellido,		
+				SUM(factura.factura_importeTotal)	AS montoTotalFacturado
+FROM				MESSI_MAS3.Factura factura
+		LEFT JOIN	MESSI_MAS3.Publicacion pub ON factura_publicacionId = pub.publicacion_id
+		LEFT JOIN  (SELECT cliente_id, cliente_apellido FROM MESSI_MAS3.Cliente
+					UNION
+					SELECT empresa_id, empresa_razonSocial FROM MESSI_MAS3.Empresa) AS cliempresa ON cliempresa.cliente_id = pub.publicacion_idUsuario
+		LEFT JOIN	MESSI_MAS3.Usuario users ON pub.publicacion_idUsuario = users.usuario_id
+WHERE	
+		(MONTH(factura.factura_fecha) = @mes1 OR MONTH(factura.factura_fecha) = @mes2 OR MONTH(factura.factura_fecha) = @mes3)	AND YEAR(factura.factura_fecha) = @anioCasteado
+GROUP BY cliempresa.cliente_id, cliempresa.cliente_apellido, users.usuario_nombreUsuario
+ORDER BY montoTotalFacturado DESC
+
+
+
+END
+GO
+
+CREATE PROCEDURE get_top5clientesConMasCompraSegunRubro(@anio NVARCHAR(255), @nroTrimestre INT, @nombreRubro NVARCHAR(255))
+AS BEGIN
+
+DECLARE @mes1 INT
+DECLARE @mes2 INT
+DECLARE @mes3 INT
+DECLARE @idRubro INT
+SELECT @idRubro = rubro_id FROM MESSI_MAS3.Rubro WHERE @nombreRubro = rubro_descripcionCorta
+DECLARE @anioCasteado int
+
+SET @anioCasteado = CONVERT(INT,@anio)
+
+IF @nroTrimestre = 1 
+BEGIN
+	SET @mes1 = 1
+	SET @mes2 = 2 
+	SET @mes3 = 3
+END
+IF @nroTrimestre = 2
+BEGIN
+	SET @mes1 = 4
+	SET @mes2 = 5 
+	SET @mes3 = 6
+END
+IF @nroTrimestre = 3
+BEGIN
+	SET @mes1 = 7
+	SET @mes2 = 8 
+	SET @mes3 = 9
+END
+IF @nroTrimestre = 4
+BEGIN
+	SET @mes1 = 10
+	SET @mes2 = 11
+	SET @mes3 = 12
+END
+
+
+SELECT TOP 5 
+		compra.compras_personaComprador_id		AS idUsuario,
+		cliente.cliente_DNI		AS documento,
+		cliente.cliente_nombre			AS nombre,
+		cliente.cliente_apellido		AS apellido,
+		COUNT(*)			AS cantidadCompras
+FROM 			MESSI_MAS3.Compra compra
+	LEFT JOIN	MESSI_MAS3.Publicacion publi ON compra.compras_publicacion_id = publi.publicacion_id
+	LEFT JOIN	MESSI_MAS3.Cliente cliente on cliente.cliente_id = compra.compras_personaComprador_id
+WHERE 
+	EXISTS (SELECT * FROM MESSI_MAS3.Rubro_x_Publicacion WHERE @idRubro = idRubro AND publi.publicacion_id = idPublicacion)
+	AND
+	YEAR(publi.publicacion_fechaFin) = @anio
+	AND
+	(MONTH(publi.publicacion_fechaFin) = @mes1 OR MONTH(publi.publicacion_fechaFin) = @mes2 OR MONTH(publi.publicacion_fechaFin) = @mes3)
+GROUP BY compra.compras_personaComprador_id, cliente.cliente_nombre, cliente.cliente_DNI, cliente.cliente_apellido
+ORDER BY cantidadCompras DESC
+END 
+GO
